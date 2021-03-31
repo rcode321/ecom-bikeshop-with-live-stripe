0710339df356ba5eca15e1b78577f347d10935ad
Raf <rafaelmendoza004@gmail.com>
2021-04-01 03:17:59 +0800

    trying a React profiler

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Profiler } from "react";
 import Directory from "../../components/directory/directory.component";
 import Hero from "../../components/hero/hero.component";
 import "./homepage-styles.scss";
 
 const Homepage = () => (
   <div className="homepage-container">
-    <Hero />
-    <Directory />
+    <Profiler
+      id="Directory"
+      onRender={(id, phase, actualDuration) => {
+        console.log({
+          id,
+          phase,
+          actualDuration,
+        });
+      }}
+    >
+      <Hero />
+      <Directory />
+    </Profiler>
   </div>
 );
 
